b62e038d409b06d0bdf0ba43eb3c1c8b04279eee
Semi-koron <6jimoto6@gmail.com>
2024-10-20 12:42:39 +0900

    fix

--- Assets/Scripts/SendMessage.cs
+++ Assets/Scripts/SendMessage.cs
@@ -9,6 +9,8 @@ public class SendMessage : MonoBehaviour
     public FetchJson fetchJson;
     public TMP_InputField inputField;
     public string id;
+
+    public string to;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +21,7 @@ public class SendMessage : MonoBehaviour
     public void Send()
     {
         string message = inputField.text;
-        fetchJson.PostMessage(message, id);
+        fetchJson.PostMessage(message, id, to);
         Debug.Log("Message: " + message);
     }
 }
